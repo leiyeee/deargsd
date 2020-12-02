701c48f61c9a7024fc4e2c60b382eae17db83e5e
leiyeee <leiye@mit.edu>
2020-12-02 03:05:24 -0500

    klkj

--- js/audio.js
+++ js/audio.js
@@ -1,10 +1,19 @@
-/*function PlaySound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.play();
-}
+$('.soundbutton').on('mousedown', function () {     
+    playWind();                                  //start wind sound
+})
+.on('mouseup', function () {                    
+    stopWind();                                  //stops the wind sound
+});
 
-function StopSound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.pause();
-    thissound.currentTime = 0;
-}*/
+
+// my full functions to start and stop
+
+function playWind () {                           //start wind audio
+  $('#wind-sound')[0].volume = 0.7;
+  $('#wind-sound')[0].load();
+  $('#wind-sound')[0].play();
+}
+function stopWind () {                           //stop the wind audio
+  $('#wind-sound')[0].pause();
+  $('#wind-sound')[0].currentTime = 0;           //resets wind to zero/beginning
+}
